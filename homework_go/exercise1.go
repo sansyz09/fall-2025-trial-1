1c2ac083188d1aaac92cd1898e2bc4998e1615d7
sansyz09 <sansyz09@gmail.com>
2025-08-29 22:38:25 +0500

    fix

--- homework_go/exercise1.go
+++ homework_go/exercise1.go
@@ -3,5 +3,5 @@ package main
 // TODO: Write a function called Greet that takes a name (string) and returns "Hello, [name]!"
 func Greet(name string) string {
 	// Here is your code
-	return "Hello, Alice!"
+	return "Hello, " + name + "!"
 }
